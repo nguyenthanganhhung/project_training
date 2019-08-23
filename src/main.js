3a5fb6e49f7e0ee1bed37559aabaf8dda352de49
nguyenthanganhhung <hungnta@lifetimetech.vn>
2019-08-23 13:37:19 +0700

    fix-conflic

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
+/* eslint-disable no-new */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
+import App from './App'
 import router from './router'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 import { store } from './store/store'
+import VueRouter from 'vue-router'
+// import FormLogin from './components/FormLogin.vue'
 // import Master from './components/Master.vue'
-import LearnFrontCss from './components/LearnFrontCss.vue'
+import LearnCss from './components/LearnCss.vue'
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+Vue.use(VueRouter)
 new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(LearnFrontCss)
+  render: h => h(LearnCss)
   // components: { App },
   // template: '<App/>'
 })
