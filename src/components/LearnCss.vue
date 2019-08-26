2029f5fa12b4e788ff8ac39c8a0cebac8034f17c
nguyenthanganhhung <hungnta@lifetimetech.vn>
2019-08-26 11:46:30 +0700

    update-name-class

--- src/components/LearnCss.vue
+++ src/components/LearnCss.vue
@@ -1,23 +1,23 @@
 <template>
     <section class="container">
       <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700|Source+Sans+Pro:300,400&display=swap" rel="stylesheet">
-      <div class="col-md-4 price-table">
-        <div class="price-table-box">
-          <div class="price-cricle-price">
-            <p class="price-table-price">$99</p>
-            <p class="price-person">/Person</p>
+      <div class="col-md-4 col-sm-6 pricingTable">
+        <div class="pricingTable-header">
+          <div class="pricingCricle-content">
+            <p class="pricingCricle-price">$99</p>
+            <p class="pricing-person">/Person</p>
           </div>
         </div>
-        <div class="price-title-basic">
-          <h2 class="price-basic">BASIC</h2>
-          <h3 class="price-plan">PRICE PLAN</h3>
+        <div class="pricing-title-basic">
+          <h2 class="pricing-basic">BASIC</h2>
+          <h3 class="pricing-plan">PRICE PLAN</h3>
         </div>
-        <div class="price-list-group">
-          <div class="price-header">
+        <div class="pricing-list-group">
+          <div class="pricingTitle">
             <h5>Subtitle goes here</h5>
             <h6>subtitle second line goes here lorem ipsum *</h6>
           </div>
-          <div class="price-list-item">
+          <div class="pricing-list-item">
             <ul class="list-group">
                 <li>
                   <P>&#9745;
@@ -46,35 +46,35 @@
                 </li>
               </ul>
           </div>
-          <div class="price-box-order">
-            <div class="price-order">
+          <div class="pricingTable-order">
+            <div class="pricing-order">
               <span><a href="#">ORDER</a></span>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-4 price-table">
-        <div class="price-table-box">
-          <div class="price-cricle-price">
-            <p class="price-table-price-advanced">$199</p>
-            <p class="price-person">/Person</p>
+      <div class="col-md-4 col-sm-6 pricingTable">
+        <div class="pricingTable-header">
+          <div class="pricingCricle-content">
+            <p class="pricingCricle-price-advanced">$199</p>
+            <p class="pricing-person">/Person</p>
           </div>
         </div>
-        <div class="price-new">
+        <div class="pricing-new">
           <img src="../assets/icon.png">
-          <!-- <span class="price-title-new">NEW</span> -->
+          <!-- <span class="pricing-title-new">NEW</span> -->
         </div>
-        <div class="price-title-advanced">
-          <h2 class="price-basic">ADVANCED</h2>
-          <h2 class="price-plan">PRICE PLAN</h2>
-          <h6 class="price-plan-text">This is the very best price quantity advanced plan *</h6>
+        <div class="pricing-title-advanced">
+          <h2 class="pricing-basic">ADVANCED</h2>
+          <h2 class="pricing-plan">PRICE PLAN</h2>
+          <h6 class="pricing-plan-text">This is the very best pricing quantity advanced plan *</h6>
         </div>
-        <div class="price-list-group">
-          <div class="price-header">
+        <div class="pricing-list-group">
+          <div class="pricingTitle">
             <h5>Subtitle goes here</h5>
             <h6>subtitle second line goes here lorem ipsum *</h6>
           </div>
-          <div class="price-list-item">
+          <div class="pricing-list-item">
             <ul class="list-group">
                 <li>
                   <P>&#9745;
@@ -103,30 +103,30 @@
                 </li>
               </ul>
           </div>
-          <div class="price-box-order">
-            <div class="price-order">
+          <div class="pricingTable-order">
+            <div class="pricing-order">
               <span><a href="#">ORDER</a></span>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-4 price-table">
-        <div class="price-table-box">
-          <div class="price-cricle-price">
-            <p class="price-table-price-life">$1999</p>
-            <p class="price-person">/Person</p>
+      <div class="col-md-4 col-sm-6 pricingTable">
+        <div class="pricingTable-header">
+          <div class="pricingCricle-content">
+            <p class="pricingCricle-price-life">$1999</p>
+            <p class="pricing-person">/Person</p>
           </div>
         </div>
-        <div class="price-title-life">
-          <h2 class="price-basic">LIFE</h2>
-          <h3 class="price-plan">TARIFF</h3>
+        <div class="pricing-title-life">
+          <h2 class="pricing-basic">LIFE</h2>
+          <h3 class="pricing-plan">TARIFF</h3>
         </div>
-        <div class="price-list-group">
-          <div class="price-header">
+        <div class="pricing-list-group">
+          <div class="pricingTitle">
             <h5>Subtitle goes here</h5>
             <h6>subtitle second line goes here lorem ipsum *</h6>
           </div>
-          <div class="price-list-item">
+          <div class="pricing-list-item">
             <ul class="list-group">
                 <li>
                   <P>&#9745;
@@ -155,8 +155,8 @@
                 </li>
               </ul>
           </div>
-          <div class="price-box-order">
-            <div class="price-order">
+          <div class="pricingTable-order">
+            <div class="pricing-order">
               <span><a href="https://www.w3schools.com/cssref/default.asp">ORDER</a></span>
             </div>
           </div>
@@ -174,20 +174,21 @@
       justify-content: center;
       flex-direction: row;
       flex-wrap: wrap;
-    .price-table::after {
+      box-sizing: border-box;
+    .pricingTable::after {
       content: '';
       clear: both;
       display: table;
-      padding-top:9rem;
-      }
-    .price-table:hover {
-      box-shadow: 10px 10px 5px grey;
     }
-    .price-table {
+    .pricingTable:hover {
+      box-shadow: 0 0 15px rgba(0,0,0,0.2) inset, 0 0 15px rgba(0,0,0,0.2);
+      border-radius: 15px;
+    }
+    .pricingTable {
       border: 1px solid #dfe5e8;
       padding: 0;
       
-      .price-table-box {
+      .pricingTable-header {
       width: 7.5rem;
       height: 7.5rem;
       border-radius: 50%;
@@ -199,27 +200,27 @@
       justify-content: center;
       flex-direction: column;
       text-align: center;
-        .price-cricle-price {
-          .price-table-price {
+        .pricingCricle-content {
+          .pricingCricle-price {
             color: #0ec7e1;
             margin: 0;
           }
-          .price-person {
+          .pricing-person {
             color: #99a9b5;
             margin: 0;
             font-family: 'Source Sans Pro', sans-serif;
           }
-          .price-table-price-advanced {
+          .pricingCricle-price-advanced {
             color: #1c94ca;
             margin: 0;
           }
-          .price-table-price-life {
+          .pricingCricle-price-life {
             color: #7264bd;
             margin: 0;
           }
         }
       }
-      .price-new::after {
+      .pricing-new::after {
         content: 'NEW';
         color: #ffffff;
         font-size: 0.75rem;
@@ -228,51 +229,51 @@
         margin-left: 0.125rem;
         text-align: center;
       }
-      .price-new {
+      .pricing-new {
         width: 66px;
         height: 66px;
-        margin-top: -2.0625rem;
+        top: -2.0625rem;
         position: absolute;
-        margin-left: 9.1875rem;
+        left: 9.1875rem;
         text-align: center;
       }
-      .price-title-basic {
+      .pricing-title-basic {
         font-size: 2.25rem;
         background-color: #00bcd4;
         color: #ffffff;
         height: 17rem;
         padding: 9rem 0 0  2.9rem;
         font-family: 'Montserrat', sans-serif;
-        .price-basic {
+        .pricing-basic {
           margin: 0;
           padding: 0;
           font-weight: bold;
         }
-        .price-plan {
+        .pricing-plan {
           margin: 0;
           padding-top: 1.3rem;
           font-weight: 200;
         }
       }
-      .price-title-advanced {
-        @extend .price-title-basic;
+      .pricing-title-advanced {
+        @extend .pricing-title-basic;
         padding: 6.5rem 0 0 2.9rem;
         background-color: #1c94ca;
-        .price-plan-text {
+        .pricing-plan-text {
           font-size: 0.875rem;
           font-family: 'Source Sans Pro', sans-serif;
           padding-top: 1rem;
         }
       }
-      .price-title-life {
-        @extend .price-title-basic;
+      .pricing-title-life {
+        @extend .pricing-title-basic;
         background-color: #7264bd;
       }
-      .price-list-group {
+      .pricing-list-group {
         background-color: #ffffff;
         color: #7b8a94;
         font-family: 'Source Sans Pro', sans-serif;
-        .price-header {
+        .pricingTitle {
           height: 6.2rem;
           h5 {
             font-size: 1.5rem;
@@ -286,7 +287,7 @@
             padding: 0.625rem 0 1.75rem 2.9rem;
           }
         }
-        .price-list-item {
+        .pricing-list-item {
           ul {
             list-style: none;
           }
@@ -300,17 +301,18 @@
             }
           }
         }
-        .price-box-order {
+        .pricingTable-order {
           height: 9.0625rem;
             display: flex;
             align-items: center;
             padding-left: 2.9rem;
-          .price-order:hover {
+          .pricing-order:hover {
             border: 1px solid #b6c6c9;
             box-shadow: 10px 10px 5px grey;
             background: #2c2e3d;
+            letter-spacing: 3px;
           }
-          .price-order {
+          .pricing-order {
             height: 3.125rem;
             width: 9.375rem;
             background-color: #b6c6c9;
